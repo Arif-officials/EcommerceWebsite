5e5f8c6535e65bf8a36042fb7009819b76f156b5
Sk Aqib Ali <122338420+Aqib-Ali08@users.noreply.github.com>
2023-12-07 10:09:09 +0530

    navbar color updated

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
     <nav>
         
       <ul>
-        <li><div className="logo"></div></li>
+        <li><div className="logo"></div> <span className="logo-text">Majestic</span></li>
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#services">Services</a></li>
